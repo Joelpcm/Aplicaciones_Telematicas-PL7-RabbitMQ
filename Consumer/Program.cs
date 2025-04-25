314be6c6c36e01b1d4c6ae925abd01434b40545a
Joel Pérez Camacho <UO289552@uniovi.es>
2025-04-25 14:54:26 +0200

    Se cambio la version del paquete de RabbitMQ de la version 7 a la ultima version de la 6 puesto que la 7 no soporta tareas sincronas. Se ejecuto el programa y se comprobo que todo funciona segun los esperado.

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,12 +1,44 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
 
 namespace Consumer
 {
     internal class Program
     {
+        // Constante con la IP
+        private const string RabbitMqHost = "192.168.0.27";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var factory = new ConnectionFactory() { HostName = RabbitMqHost };
+            // Para realizar el envío de información desde el cliente
+            // hay que crear una conexión con el servidor
+            using (var connection = factory.CreateConnection())
+            {
+                // A continuación hay que crear un canal,
+                // que es una abstracción para enviar y recibir información
+                using (var channel = connection.CreateModel())
+                {
+                    //En primer lugar se declara una cola
+                    channel.QueueDeclare("ColaAT", false, false, false, null);
+
+                    // Se crea un consumidor
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body.ToArray());
+                        Console.WriteLine("Recibido {0}", message);
+                    };
+                    channel.BasicConsume(queue: "ColaAT",
+                                      autoAck: true,
+                                      consumer: consumer);
+
+                    // Bloquea el consumidor a la espera de recibir mensajes
+                    Console.WriteLine("Press enter to exit");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,13 +1,36 @@
-﻿using RabbitMQ.Client.Events;
-using RabbitMQ.Client;
+﻿using RabbitMQ.Client;
+using System.Text;
 
 namespace Producer
 {
     internal class Program
     {
+        // Constante con la IP
+        private const string RabbitMqHost = "192.168.0.27";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var factory = new ConnectionFactory() { HostName = RabbitMqHost };
+            // Para realizar el envío de información desde el cliente
+            // hay que crear una conexión con el servidor
+            using (var connection = factory.CreateConnection())
+            {
+                // A continuación hay que crear un canal,
+                // que es una abstracción para enviar y recibir información
+                using (var channel = connection.CreateModel())
+                {
+                    // En primer lugar se declara una cola
+                    channel.QueueDeclare("ColaAT", false, false, false, null);
+
+                    // Se crea un mensaje
+                    string message = DateTime.Now.ToString() + " - Mensaje de prueba";
+                    byte[] body = Encoding.UTF8.GetBytes(message);
+
+                    // Se publica un mensaje en la cola
+                    channel.BasicPublish("", "ColaAT", null, body);
+                    Console.WriteLine("Enviado el mensaje: {0}", message);
+                }
+            }
         }
     }
 }
