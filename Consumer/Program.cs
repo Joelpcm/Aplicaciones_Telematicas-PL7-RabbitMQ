cb602cf45dda58d69503d6336344da950a4e7bf6
Joel Pérez Camacho <UO289552@uniovi.es>
2025-04-25 17:38:50 +0200

    Implementada publicación suscripción con tópicos. Hizo falta eliminar el intercambiador de alertas de tipo fanout del programa RabbitMQ para poder añadir otro.

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -8,6 +8,11 @@ namespace Consumer
     {
         // Constante con la IP
         private const string RabbitMqHost = "192.168.0.27";
+
+        // Para el patrón publicación/subscripción
+        const string BINDING_KEY = "alerta.*";
+        const string EXCHANGE = "Alertas";
+
         static void Main(string[] args)
         {
             var factory = new ConnectionFactory() { HostName = RabbitMqHost };
@@ -19,57 +24,30 @@ namespace Consumer
                 // que es una abstracción para enviar y recibir información
                 using (var channel = connection.CreateModel())
                 {
-                    // Se crea un intercambiador de tipo fanout denominado "Alertas"
-                    channel.ExchangeDeclare("Alertas", "fanout");
+                    // Se declara un intercambiador de tipo topic denominado "Alertas"
+                    channel.ExchangeDeclare(EXCHANGE, "topic");
 
                     // Se crea una cola temporal
-                    var queueName = channel.QueueDeclare().QueueName;
+                    var queueName = channel.QueueDeclare();
 
-                    // Se enlaza la cola con el intercambiador
-                    channel.QueueBind(queueName, "Alertas", "");
+                    // Se desean recibir todas las alertas
+                    channel.QueueBind(queueName, EXCHANGE, BINDING_KEY);
 
-                    // Se crea un consumidor
+                    Console.WriteLine($"Esperando mensajes con patrón '{BINDING_KEY}'...");
+
+                    // Consumir mensajes
                     var consumer = new EventingBasicConsumer(channel);
                     consumer.Received += (model, ea) =>
                     {
                         var body = ea.Body.ToArray();
                         var message = Encoding.UTF8.GetString(body);
-                        Console.WriteLine("Recibido {0}", message);
-
-                        // Dividimos el mensaje para obtener el número
-                        string[] parts = message.Split('-');
-                        if (parts.Length >= 3)
-                        {
-                            // Extraemos el número y lo convertimos a entero
-                            string numberPart = parts[2].Trim();
-                            if (int.TryParse(numberPart, out int taskNumber))
-                            {
-                                Console.WriteLine($"Ejecutando tarea número: {taskNumber}");
-
-                                // Simulamos la ejecución de la tarea
-                                Console.WriteLine($"Iniciando ejecución de la tarea {taskNumber}...");
-                                Thread.Sleep(taskNumber * 1000); // Simulamos el tiempo de ejecución según el número
-                                Console.WriteLine($"Tarea {taskNumber} completada exitosamente.");
-                            }
-                            else
-                            {
-                                Console.WriteLine("No se pudo extraer un número válido del mensaje.");
-                            }
-                        }
-
-                        // ACK del mensaje
-                        channel.BasicAck(ea.DeliveryTag, false);
-
+                        Console.WriteLine($"Recibido: '{message}' con clave de enrutado '{ea.RoutingKey}'");
                     };
-                    // Para que la división de mensajes no se realice al principio
-                    channel.BasicQos(0, 1, false);
-                    // Se inicia el consumidor
-                    channel.BasicConsume(queue: "ColaTareas",
-                                      autoAck: false, //valor false = consumidores deben enviar mensaje cuando terminan de procesar un mensaje
-                                      consumer: consumer);
+
+                    channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
 
                     // Bloquea el consumidor a la espera de recibir mensajes
-                    Console.WriteLine("Press enter to exit");
+                    Console.WriteLine("Presiona [Enter] para salir.");
                     Console.ReadLine();
                 }
             }
--- Producer/Program.cs
+++ Producer/Program.cs
@@ -8,6 +8,10 @@ namespace Producer
         // Constante con la IP
         private const string RabbitMqHost = "192.168.0.27";
 
+        // Para el patrón publicación/subscripción
+        const string ROUTING_KEY = "alerta.crítica";
+        const string EXCHANGE = "Alertas";
+
         static void Main(string[] args)
         {
             var factory = new ConnectionFactory() { HostName = RabbitMqHost };
@@ -19,37 +23,22 @@ namespace Producer
                 // que es una abstracción para enviar y recibir información
                 using (var channel = connection.CreateModel())
                 {
-                    // Los mensajes se establecen como persistentes
-                    var properties = channel.CreateBasicProperties();
-                    properties.Persistent = true;
+                    // Se declara un intercambiador de tipo topic denominado "Alertas"
+                    channel.ExchangeDeclare(EXCHANGE, "topic");
 
-                    // En primer lugar se declara una cola
-                    bool durable = true;
-                    channel.QueueDeclare("ColaTareas", durable, false, false, null);
+                    // Claves de enrutado de ejemplo
+                    string[] routingKeys = { "alerta.info", "alerta.crítica", "alerta.aviso" };
 
-                    // Para hacer 10 tareas de 1 segundo y 1 de 10 segundos
-                    int Number_1 = 1;
-                    int Number_2 = 10;
+                    // Enviar mensajes con diferentes claves de enrutado
+                    foreach (var routingKey in routingKeys)
+                    {
+                        string message = $"Mensaje para {routingKey}";
+                        byte[] body = Encoding.UTF8.GetBytes(message);
 
-                    string message;
-                    byte[] body;
-
-                    for (int i = 0; i < 10; i++)
-                    {    
-                        // Se crea un mensaje que incluye el número aleatorio
-                        message = $"{DateTime.Now} - Mensaje de prueba - {Number_1.ToString()}";
-                        body = Encoding.UTF8.GetBytes(message);
-                        // Se publica un mensaje en la cola
-                        channel.BasicPublish("", "ColaTareas", properties, body);
-                        Console.WriteLine("Enviado el mensaje: {0}", message);
+                        // Publicar el mensaje con la clave de enrutado
+                        channel.BasicPublish(EXCHANGE, routingKey, null, body);
+                        Console.WriteLine($"Enviado: '{message}' con clave de enrutado '{routingKey}'");
                     }
-                    // Se crea un mensaje que incluye el número
-                    message = $"{DateTime.Now} - Mensaje de prueba - {Number_2.ToString()}";
-                    body = Encoding.UTF8.GetBytes(message);
-
-                    // Se publica un mensaje en la cola
-                    channel.BasicPublish("", "ColaTareas", properties, body);
-                    Console.WriteLine("Enviado el mensaje: {0}", message);
                 }
             }
         }
